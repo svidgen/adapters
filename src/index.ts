1204198b30bebda24dbd0f22d2ad7d2a635e16b9
Jonathan Wire <iambipedal@gmail.com>
2021-12-18 10:06:31 -0600

    stash

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { ulid } from 'ulid';
 
+type ModelPK<PK extends string> = {
+	[key in PK]: string
+};
+
 export function validateHasId<T>(item: T, pk: string) {
 	if (typeof (item as any)[pk] === 'undefined') {
 		throw new Error(`${item} does not have the expected PK: (${pk}).`);
@@ -8,20 +12,36 @@ export function validateHasId<T>(item: T, pk: string) {
 
 export class Collection<
 	PK extends string = 'id',
-	T extends {[key in PK]: any} = {[key in PK]: any} & Record<string, any>
+	T extends ModelPK<PK> = ModelPK<PK> & Record<string, any>
 > {
 	validate: (item: T, pk: PK) => any;
 	pk: PK;
-	items: T[];
+	private items: T[];
 	// indexes: Collection<T>[];
 
 	constructor({
 		validate = validateHasId,
 		pk = 'id' as PK,
 		items = []
-	}: Partial<Collection<PK, T>> = {}) {
+	}: (Partial<Collection<PK, T>> & { items?: T[] }) = {}) {
 		this.validate = validate;
 		this.pk = pk;
-		this.items = items as T[];
+		this.items = items;
 	}
+
+	async get(pk: PK) {
+
+	}
+
+	async put(item: Omit<T, PK> & Partial<ModelPK<PK>>): Promise<T> {
+		const itemCopy = {...item} as ModelPK<PK>;
+
+		const pk = itemCopy[this.pk];
+		if (!itemCopy[this.pk]) {
+			itemCopy[this.pk] = ulid();
+		}
+
+		return itemCopy as T;
+	}
+
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,51 +1,69 @@
 import { Collection } from '../src';
 
+function makeItems<PK extends string = 'id'>(ids: string[], idField: PK = 'id' as PK): ({[key in PK]: any} & {name: string})[] {
+	return ids.map(id => ({
+		[idField]: id,
+		name: id + ' name'
+	})) as ({[key in PK]: any} & {name: string})[];
+};
+
 describe('Collection', () => {
-	test('can be constructed empty with no specific PK', () => {
-		const c = new Collection();
-		expect(c.pk).toEqual('id');
-	});
-
-	test('can be constructed with a specific PK', () => {
-		const c = new Collection({pk: 'notId'});
-		expect(c.pk).toEqual('notId');
-	});
-
-	test('can be constructed with type', () => {
-		type Model = {
-			id: string;
-			name: string;
-		}
-		const c = new Collection<"id", Model>();
-		expect(c.pk).toEqual('id');
-	});
-
-	test('can be constructed with a validator', () => {
-		type Model = {
-			id: string;
-			name: string;
-		}
-		const c = new Collection({
-			validate: (item: Model, pk = 'id') => {}
+	describe("can be constructed with", () => {
+		test('no specific PK', () => {
+			const c = new Collection();
+			expect(c.pk).toEqual('id');
 		});
-		expect(c.pk).toEqual('id');
-	});
 
-	test('can be constructed with a validator and pk', () => {
-		type Model = {
-			notId: string;
-			name: string;
-		}
-		const c = new Collection({
-			pk: 'notId',
-			validate: (item: Model, pk = 'notId') => {}
+		test('a specific PK', () => {
+			const c = new Collection({pk: 'notId'});
+			expect(c.pk).toEqual('notId');
 		});
-		expect(c.pk).toEqual('notId');
-	});
 
+		test('type', () => {
+			type Model = {
+				id: string;
+				name: string;
+			}
+			const c = new Collection<"id", Model>();
+			expect(c.pk).toEqual('id');
+		});
 
-	test('can be constructed with an empty array', () => {
-		const c = new Collection({items: []});
-		expect(c.pk).toEqual('id');
+		test('a validator', () => {
+			type Model = {
+				id: string;
+				name: string;
+			}
+			const c = new Collection({
+				validate: (item: Model, pk = 'id') => {}
+			});
+			expect(c.pk).toEqual('id');
+		});
+
+		test('a validator and pk', () => {
+			type Model = {
+				notId: string;
+				name: string;
+			}
+			const c = new Collection({
+				pk: 'notId',
+				validate: (item: Model, pk = 'notId') => {}
+			});
+			expect(c.pk).toEqual('notId');
+		});
+
+		test('an empty array', () => {
+			const c = new Collection({items: []});
+			expect(c.pk).toEqual('id');
+		});
+
+		test('array of objects with ids', () => {
+			const c = new Collection({items: makeItems(['a','b','c'])});
+			expect(c.pk).toEqual('id');
+		});
+
+		test('array of objects with ids', () => {
+			const c = new Collection({items: makeItems(['a','b','c'], "differentPK"), pk: "differentPK"});
+			expect(c.pk).toEqual('differentPK');
+		});
 	});
 });
